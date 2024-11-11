076bdb09aa67de8e41038c96cb05257d6c998ee4
nguyenhung1402 <21522118@ms.uit.edu.vn>
2024-11-11 23:23:09 +0700

    test

--- user/server.js
+++ user/server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8800;
 const server = app.listen(PORT, () => {
   console.log(`Test`);
   console.log(`Dev Server running on port: ${PORT}`);
-  console.log(`New jekins test`);
+  console.log(`New jekins final test`);
 });
 
 export default server;
